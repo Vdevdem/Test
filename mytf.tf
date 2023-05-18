334d42154b799c68cedfbd719104720e41e6515b
Volodymyr Medvezhynskyi <volodymyr_medvezhynskyi@epam.com>
2023-05-18 15:03:35 +0300

    fix output

--- mytf.tf
+++ mytf.tf
@@ -10,12 +10,16 @@ resource "aws_instance" "my_instance" {
   key_name               = "demo"
 }
 
-resource "aws_eip" "static_id" {
+resource "aws_eip" "static_eip" {
   instance = aws_instance.my_instance.id
 }
 
 output "instance_id" {
   value = aws_instance.my_instance.id
+}
+
+output "instance_public_ip" {
+  value = aws_eip.static_eip.public_ip
 }
 
 resource "aws_security_group" "my_webserver" {
